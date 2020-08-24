91f8d59b8517e94571ac6e7705e314c47395ac71
Yuri Furquim <yuripatrick347@gmail.com>
2020-08-24 14:00:39 -0300

    Project created

---
+++ src/main/java/com/psm/projectSpringMongoDB/ProjectSpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.psm.projectSpringMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psm/projectSpringMongoDB/ProjectSpringMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psm.projectSpringMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
