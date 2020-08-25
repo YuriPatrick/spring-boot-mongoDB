1e9b1d22468974c7c94dbfe78f6a82def27f02ad
Yuri Furquim <yuripatrick347@gmail.com>
2020-08-25 07:51:36 -0300

    Projection of the author's data with DTO

--- src/main/java/com/psm/projectSpringMongoDB/config/Instatiantion.java
+++ src/main/java/com/psm/projectSpringMongoDB/config/Instatiantion.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.psm.projectSpringMongoDB.domain.Post;
 import com.psm.projectSpringMongoDB.domain.User;
+import com.psm.projectSpringMongoDB.dto.AuthorDTO;
 import com.psm.projectSpringMongoDB.repository.PostRepository;
 import com.psm.projectSpringMongoDB.repository.UserRepository;
 
@@ -17,28 +18,28 @@ public class Instatiantion implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2020"), "partiu viajem", "Vou viajar para Argentina. Abraços", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Acordei feliz hose!", maria);
-		
-		
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
