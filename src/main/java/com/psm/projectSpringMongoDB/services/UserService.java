917798b073b523f156b9a5dee09a656d5440d2af
Yuri Furquim <yuripatrick347@gmail.com>
2020-08-24 21:21:28 -0300

    Insertion and deletion with POST and DELETE

--- src/main/java/com/psm/projectSpringMongoDB/services/UserService.java
+++ src/main/java/com/psm/projectSpringMongoDB/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.psm.projectSpringMongoDB.domain.User;
+import com.psm.projectSpringMongoDB.dto.UserDTO;
 import com.psm.projectSpringMongoDB.repository.UserRepository;
 import com.psm.projectSpringMongoDB.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,18 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
