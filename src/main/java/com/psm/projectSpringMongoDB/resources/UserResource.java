db960afad44c74cde65dd03dec2340bf3510566e
Yuri Furquim <yuripatrick347@gmail.com>
2020-08-25 08:04:16 -0300

    Endpoint to return a user's posts

--- src/main/java/com/psm/projectSpringMongoDB/resources/UserResource.java
+++ src/main/java/com/psm/projectSpringMongoDB/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.psm.projectSpringMongoDB.domain.Post;
 import com.psm.projectSpringMongoDB.domain.User;
 import com.psm.projectSpringMongoDB.dto.UserDTO;
 import com.psm.projectSpringMongoDB.services.UserService;
@@ -58,5 +59,10 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
