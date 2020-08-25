deb0117f568f0474e1dc85154e266271733531a9
Yuri Furquim <yuripatrick347@gmail.com>
2020-08-25 08:13:48 -0300

    Getting a post by id

---
+++ src/main/java/com/psm/projectSpringMongoDB/services/PostService.java
@@ -0,0 +1,23 @@
+package com.psm.projectSpringMongoDB.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.psm.projectSpringMongoDB.domain.Post;
+import com.psm.projectSpringMongoDB.repository.PostRepository;
+import com.psm.projectSpringMongoDB.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
