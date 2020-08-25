9652e6da91b18f8ad18967af0f6d0bd840b5ba2c
Yuri Furquim <yuripatrick347@gmail.com>
2020-08-25 08:41:02 -0300

    Adding comments to posts

--- src/main/java/com/psm/projectSpringMongoDB/domain/Post.java
+++ src/main/java/com/psm/projectSpringMongoDB/domain/Post.java
@@ -1,12 +1,15 @@
 package com.psm.projectSpringMongoDB.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.psm.projectSpringMongoDB.dto.AuthorDTO;
+import com.psm.projectSpringMongoDB.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,7 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -63,12 +67,20 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	public AuthorDTO  getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(AuthorDTO  author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
